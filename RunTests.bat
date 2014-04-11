90e0960f7a6f8348f7b75b05cc5b012246fca584
Timothy Fluehr <tfluehr@gmail.com>
2014-04-11 14:03:29 -0400

    and more files

---
+++ RunTests.bat
@@ -0,0 +1,1 @@
+phantomjs\phantomjs.exe "phantomjs_runner\JQUnit-runner.js" --QUnitFileName qunit.js --TestsPath . --junit c:\dev\qunit\qunit-results.xml
---
+++ tests.js
@@ -0,0 +1,7 @@
+test( "hello test", function() {
+  ok( 1 == "1", "Passed!" );
+});
+
+test("Another test", function() {
+   ok(1 == "2", "Passed!");
+});
