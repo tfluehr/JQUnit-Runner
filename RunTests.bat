10d6c612a788ead8580a4356dff1eb3c122d6f4f
Timothy Fluehr <tfluehr@gmail.com>
2014-04-16 10:14:42 -0400

    file cleanup

--- RunTests.bat
+++
@@ -1,1 +0,0 @@
-phantomjs\phantomjs.exe "phantomjs_runner\JQUnit-runner.js" --Debug true --QUnitFileName qunit.js --OutputProcessor Output/nunit.js --Test . --junit qunit-results.xml
--- tests.Test.js
+++
@@ -1,7 +0,0 @@
-test( "hello test", function() {
-  ok( 1 == "1", "1 Equals 1" );
-});
-
-test("Another test", function() {
-   ok(1 == "2", "1 Equals 2");
-});
